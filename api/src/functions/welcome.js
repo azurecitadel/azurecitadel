b78127fb5f11319839f5a748b124100c00888109
Richard Cheney <richeney@microsoft.com>
2025-03-22 20:00:01 +0000

    API function

---
+++ api/src/functions/welcome.js
@@ -0,0 +1,15 @@
+const { app } = require('@azure/functions');
+
+app.http('welcome', {
+    // methods: ['GET', 'POST'],
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        // context.log(`Http function processed request for url "${request.url}"`);
+
+        // const name = request.query.get('name') || await request.text() || 'world';
+
+        // return { body: `Hello, ${name}!` };
+        return { body: `Hello, this is the Azure Citadel website!` };
+    }
+});
